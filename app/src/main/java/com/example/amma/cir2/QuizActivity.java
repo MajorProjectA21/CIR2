4f38f819649549ea08195ebf2761557020d6baa8
kishnaprasad <krishnaprasadpambavasan@gmail.com>
2019-04-12 03:55:09 +0530

    29 -03 kp

--- app/src/main/java/com/example/amma/cir2/QuizActivity.java
+++ app/src/main/java/com/example/amma/cir2/QuizActivity.java
@@ -1,13 +1,16 @@
 package com.example.amma.cir2;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,9 +22,10 @@ public class QuizActivity extends AppCompatActivity {
     DatabaseReference database;
     TextView tvQuestion, tvTime, tvCountQuestion;
     Button bOption1, bOption2, bOption3, bOption4;
-    int total = 1;
+    int total = 0;
     int correct = 0;
-
+    int incorrect = 0;
+    CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,69 +41,247 @@ public class QuizActivity extends AppCompatActivity {
         bOption4 = findViewById(R.id.button4);
         updateQuestion();
 
+
+
+
     }
 
     public void updateQuestion() {
-        database = FirebaseDatabase.getInstance().getReference().child("Questions").child(String.valueOf(total));
-        database.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                final questionClass question = dataSnapshot.getValue(questionClass.class);
-                tvQuestion.setText(question.getQuestion());
-                bOption1.setText(question.getOption1());
-                bOption2.setText(question.getOption2());
-                bOption3.setText(question.getOption3());
-                bOption4.setText(question.getOption4());
-                bOption1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (bOption1.getText().toString().equals(question.getAnswer())) {
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    correct++;
-                                    bOption1.setBackgroundColor(Color.GREEN);
-                                    updateQuestion();
-                                }
-                            },1500);
-                        }
-                        else{
+        total++;
+        if (total > 6) {//total no of questions
+            Intent intent = new Intent(QuizActivity.this, quizResultActivity.class);
+            intent.putExtra("total", String.valueOf(total));
+            intent.putExtra("correct", String.valueOf(correct));
+            intent.putExtra("incorrect", String.valueOf(incorrect));
+            startActivity(intent);
+
+//result activity
+        } else {
+            try {
+                database = FirebaseDatabase.getInstance().getReference().child("Questions").child(String.valueOf(total));
+            } catch (Exception e) {
+                Toast.makeText(QuizActivity.this, "Error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+            database.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    final questionClass question;
+
+                    question = dataSnapshot.getValue(questionClass.class);
+                    tvQuestion.setText(question.getQuestion());
+                    bOption1.setText(question.getOption1());
+                    bOption2.setText(question.getOption2());
+                    bOption3.setText(question.getOption3());
+                    bOption4.setText(question.getOption4());
+                    reverseTimer(60, tvTime);
+
+                    bOption1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tvCountQuestion.setText(String.valueOf(total));
+                            if (bOption1.getText().toString().equals(question.getAnswer())) {
+                                bOption1.setBackgroundColor(Color.GREEN);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        correct++;
+                                        bOption1.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            } else {
+                                incorrect++;
                                 bOption1.setBackgroundColor(Color.RED);
-                            if (bOption2.getText().toString().equals(question.getAnswer())) {
-bOption2.setBackgroundColor(Color.GREEN);
-                            }
-                            else if (bOption3.getText().toString().equals(question.getAnswer())){
-                                bOption3.setBackgroundColor(Color.GREEN);
-                            }
-                            else if(bOption4.getText().toString().equals(question.getAnswer())){
-                                bOption4.setBackgroundColor(Color.GREEN);
-                            }
-                            Handler handler = new Handler();
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    bOption1.setBackgroundColor(Color.parseColor("#03A9F4"));
-                                    bOption2.setBackgroundColor(Color.parseColor("#03A9F4"));
-                                    bOption3.setBackgroundColor(Color.parseColor("#03A9F4"));
-                                    bOption4.setBackgroundColor(Color.parseColor("#03A9F4"));
-
-                                    updateQuestion();
+                                if (bOption2.getText().toString().equals(question.getAnswer())) {
+                                    bOption2.setBackgroundColor(Color.GREEN);
+                                } else if (bOption3.getText().toString().equals(question.getAnswer())) {
+                                    bOption3.setBackgroundColor(Color.GREEN);
+                                } else if (bOption4.getText().toString().equals(question.getAnswer())) {
+                                    bOption4.setBackgroundColor(Color.GREEN);
                                 }
-                            },1500);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        bOption1.setBackgroundResource(R.drawable.option);
+                                        bOption2.setBackgroundResource(R.drawable.option);
+                                        bOption3.setBackgroundResource(R.drawable.option);
+                                        bOption4.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            }
                         }
-                    }
-                });
+                    });
+                    //button2 click
+                    bOption2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tvCountQuestion.setText(String.valueOf(total));
+                            if (bOption2.getText().toString().equals(question.getAnswer())) {
+                                bOption2.setBackgroundColor(Color.GREEN);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        correct++;
+                                        bOption2.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            } else {
+                                incorrect++;
+                                bOption2.setBackgroundColor(Color.RED);
+                                if (bOption1.getText().toString().equals(question.getAnswer())) {
+                                    bOption1.setBackgroundColor(Color.GREEN);
+                                } else if (bOption3.getText().toString().equals(question.getAnswer())) {
+                                    bOption3.setBackgroundColor(Color.GREEN);
+                                } else if (bOption4.getText().toString().equals(question.getAnswer())) {
+                                    bOption4.setBackgroundColor(Color.GREEN);
+                                }
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+//                                        bOption1.setBackgroundColor(Color.parseColor("#03A9F4"));
+//                                        bOption2.setBackgroundColor(Color.parseColor("#03A9F4"));
+//                                        bOption3.setBackgroundColor(Color.parseColor("#03A9F4"));
+//                                        bOption4.setBackgroundColor(Color.parseColor("#03A9F4"));
+                                        bOption1.setBackgroundResource(R.drawable.option);
+                                        bOption2.setBackgroundResource(R.drawable.option);
+                                        bOption3.setBackgroundResource(R.drawable.option);
+                                        bOption4.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
 
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            }
+                        }
+                    });
+                    //button 3
+                    bOption3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tvCountQuestion.setText(String.valueOf(total));
+                            if (bOption3.getText().toString().equals(question.getAnswer())) {
+                                bOption3.setBackgroundColor(Color.GREEN);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        correct++;
+                                        bOption3.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            } else {
+                                incorrect++;
+                                bOption3.setBackgroundColor(Color.RED);
+                                if (bOption1.getText().toString().equals(question.getAnswer())) {
+                                    bOption1.setBackgroundColor(Color.GREEN);
+                                } else if (bOption2.getText().toString().equals(question.getAnswer())) {
+                                    bOption2.setBackgroundColor(Color.GREEN);
+                                } else if (bOption4.getText().toString().equals(question.getAnswer())) {
+                                    bOption4.setBackgroundColor(Color.GREEN);
+                                }
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        bOption1.setBackgroundResource(R.drawable.option);
+                                        bOption2.setBackgroundResource(R.drawable.option);
+                                        bOption3.setBackgroundResource(R.drawable.option);
+                                        bOption4.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
 
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            }
+                        }
+                    });
+                    //button 4
+                    bOption4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tvCountQuestion.setText(String.valueOf(total));
+                            if (bOption4.getText().toString().equals(question.getAnswer())) {
+                                bOption4.setBackgroundColor(Color.GREEN);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        correct++;
+                                        bOption4.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            } else {
+                                incorrect++;
+                                bOption4.setBackgroundColor(Color.RED);
+                                if (bOption1.getText().toString().equals(question.getAnswer())) {
+                                    bOption1.setBackgroundColor(Color.GREEN);
+                                } else if (bOption2.getText().toString().equals(question.getAnswer())) {
+                                    bOption2.setBackgroundColor(Color.GREEN);
+                                } else if (bOption3.getText().toString().equals(question.getAnswer())) {
+                                    bOption3.setBackgroundColor(Color.GREEN);
+                                }
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        bOption1.setBackgroundResource(R.drawable.option);
+                                        bOption2.setBackgroundResource(R.drawable.option);
+                                        bOption3.setBackgroundResource(R.drawable.option);
+                                        bOption4.setBackgroundResource(R.drawable.option);
+                                        countDownTimer.cancel();
+                                        updateQuestion();
+                                    }
+                                }, 1500);
+                            }
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    public void reverseTimer(int seconds, final TextView textView) {
+        countDownTimer = new CountDownTimer(seconds * 1000 + 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+                textView.setText(String.format("%02d", minutes)
+                        + ":" + String.format("%02d", seconds));
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onFinish() {
+                textView.setText("--:--");
+                Intent intent = new Intent(QuizActivity.this, quizResultActivity.class);
+                intent.putExtra("total", String.valueOf(total));
+                intent.putExtra("correct", String.valueOf(correct));
+                intent.putExtra("incorrect", String.valueOf(incorrect));
             }
-        });
+        }.start();
     }
 
 }
---
+++ app/src/main/java/com/example/amma/cir2/quizResultActivity.java
@@ -0,0 +1,29 @@
+package com.example.amma.cir2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class quizResultActivity extends AppCompatActivity {
+
+    TextView t1,t2,t3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_result);
+        t1 = findViewById(R.id.text_view_total_questions_value);
+        t2 = findViewById(R.id.text_view_Correct_Answers_value);
+        t3 = findViewById(R.id.text_view_Wrong_Answers_value);
+
+        Intent intent = getIntent();
+
+        String questions = intent.getStringExtra("total");
+        String correct = intent.getStringExtra("correct");
+        String wrong = intent.getStringExtra("wrong");
+
+        t1.setText(questions);
+        t2.setText(correct);
+        t3.setText(wrong);
+    }
+}
